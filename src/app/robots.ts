5c12e51a4f30053c244be1e1ef89da1ae12813c0
jayparashar10 <jayvparashar@gmail.com>
2026-06-10 15:48:32 +0530

    Add robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://ssv-healthcare-a61n.vercel.app/sitemap.xml",
+  };
+}
