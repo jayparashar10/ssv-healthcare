7aaa133d6e6a1d3b256cbf1713c80dad74e4492e
jayparashar10 <jayvparashar@gmail.com>
2026-06-10 15:32:08 +0530

    Fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,12 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://ssv-healthcare-a61n.vercel.app/",
+      url: "https://ssv-healthcare-a61n.vercel.app",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
   ];
 }
